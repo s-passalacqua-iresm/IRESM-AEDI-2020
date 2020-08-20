ba4b12eae3a3f0c8ca34e4734ae85be3b9ff444a
LucianoBartomioli <69654179+LucianoBartomioli@users.noreply.github.com>
2020-08-20 17:53:19 -0300

    Tp

---
+++ CeroPositivoNegativo.py
@@ -0,0 +1,10 @@
+num1=float(input("Ingrese un numero: "))
+
+if num1 > 0:
+    print("El numero es positivo")
+
+elif num1==0:
+    print("El numero es igual a cero")
+
+else:
+    print("El numero es negativo")
---
+++ SumaPrimIgualTer.py
@@ -0,0 +1,10 @@
+num1=float(input("Ingrese el primer número: "))
+num2=float(input("Ingrese el segundo número: "))
+num3=float(input("Ingrese el tercer número: "))
+suma=num1+num2
+
+if suma == num3:
+    print("La suma es igual al tercero")
+
+else:
+    print("La suma es distinta al tercero")
